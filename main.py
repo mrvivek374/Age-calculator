20c0a61c972f7410072a7f6617bcf836ba97be6a
mrvivek374 <manchalavivek07@gmail.com>
2026-07-22 11:57:29 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Age Caluculator
+
+your_birthyear=int(input("Enter your birth year: "))
+
+current_year=int(input("Enter current year: "))
+
+age=current_year-your_birthyear
+
+print("your age is: ",age,"Years")
